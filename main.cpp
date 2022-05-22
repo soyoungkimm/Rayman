39e53b540b410f9b90436a7201d9ee2bdf7dc7b4
soyoungkimm <ksoyoung09@gmail.com>
2022-05-22 16:23:44 +0900

    기능추가 : 레이맨 흑백 출력
    레이맨 이미지 데이터 동적할당
    이미지 자료 구조체 cel_data 추가
    put_cel(이미지 출력 함수), load_cel(이미지 로드 함수) 추가

--- main.cpp
+++ main.cpp
@@ -6,17 +6,62 @@
 // 이름: 교수 윤형태, 겜지기 17기 최지웅(OpenGl 변환)
 //==============================================================================
 #include <iostream>
-
 #include <stdio.h>
 #include <stdlib.h>
 #include "glut.h"
 #include "glext.h"
+#include <string.h>
 
 #define Wsize 320
 #define Hsize 200
 
 float Wsize2 = Wsize / 2.;
 float Hsize2 = Hsize / 2.;
+
+int rx = 10, ry = 10; // 레이맨의 현재 좌표
+
+// cel 구조체
+struct cel_data
+{
+    short width; // 가로 길이
+    short height; // 세로 길이
+    unsigned char* imageData; // 이미지 데이터
+};
+cel_data rayman;
+
+
+// 동적할당 해제 함수
+void glutLeaveMainLoop()
+{
+    free(rayman.imageData);
+}
+
+
+// 그림 자료를 읽는 함수
+void load_cel(char* filename, cel_data* cel)
+{
+    // 파일 기술자
+    FILE* pFile;
+
+    // 파일 열기
+    char fileLocation[100] = "./data/";
+    strcat(fileLocation, filename);
+    fopen_s(&pFile, fileLocation, "rb"); // 파일 open
+
+    // 이미지의 가로길이와 세로길이를 가져옴
+    cel->width = getc(pFile);
+    getc(pFile);
+    cel->height = getc(pFile);
+
+    // 파일 읽어오기
+    cel->imageData = (unsigned char*)malloc(sizeof(unsigned char) * (cel->width * cel->height));
+    fread(cel->imageData, sizeof(unsigned char), cel->width * cel->height, pFile);
+
+    // 파일 닫기
+    fclose(pFile);
+}
+
+
 
 //------------------------------------------------------------------------------
 // set_pixel : 점 찍는 함수
@@ -31,6 +76,19 @@ void set_pixel(int x, int y, unsigned char color)
 }
 
 
+// x, y 좌표에 cel그림 그리는 함수
+void put_cel(int x, int y, cel_data* cel)
+{
+    int count = 0;
+    for (int i = 0; i < cel->height; i++) {
+        for (int j = 0; j < cel->width; j++) {
+            set_pixel(x + j, y + i, cel->imageData[count]);
+            count++;
+        }
+    }
+}
+
+
 //------------------------------------------------------------------------------
 // Render : 화면에 그래픽 출력
 //------------------------------------------------------------------------------
@@ -42,25 +100,34 @@ void Render()
 
     glBegin(GL_POINTS); // 그래픽 시작
 
-    set_pixel(50, 50, 255);     // 50,50 좌표에 255밝기로 점 출력
+    // 레이맨 출력
+    put_cel(rx, ry, &rayman);
 
     glEnd();            // 그래픽 끝
 
     glFlush();  // 지금까지의 내용을 실제 출력
 }
 
+
 //------------------------------------------------------------------------------
 // Main : 메인
 //------------------------------------------------------------------------------
 int main()
 {
+    // atexit : 종료할 때 수행하는 함수 
+    atexit(glutLeaveMainLoop);
+
+    // 레이맨 이미지 읽어오기
+    char fileName[12] = "STAND0.CEL";
+    load_cel(fileName, &rayman);
+
     glutInitDisplayMode(GLUT_RGBA); // RGB 그래픽모드 지정
 
     glutInitWindowPosition(0, 0);            // 창위치
     glutInitWindowSize(Wsize, Hsize);       // 창크기
     glutCreateWindow("Gamejigi DOS Rayman 교육");     // 창제목
 
-    glClearColor(0.0, 0.0, 1.0, 0.0);  // R,G,B,A 배경:파란색 
+    glClearColor(0.0, 0.0, 0.0, 0.0);  // R,G,B,A 배경:파란색
 
     glutDisplayFunc(Render); // 화면출력
 
