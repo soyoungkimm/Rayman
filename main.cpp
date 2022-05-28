104daa27447408d879e17c83f6b19dda9849d846
soyoungkimm <ksoyoung09@gmail.com>
2022-05-28 15:51:07 +0900

    기능 추가 : 레이맨 미러링과 타이머 추가
    rayman_dir 변수 추가하고 put_cel함수 수정하여 미러링 구현
    Timer 함수 추가해서 레이맨 x좌표로 100 ~ 200 반복 이동하게 함

--- main.cpp
+++ main.cpp
@@ -19,7 +19,10 @@
 float Wsize2 = Wsize / 2.;
 float Hsize2 = Hsize / 2.;
 
-int rx = 150, ry = 150; // 레이맨의 현재 좌표
+int rx = 80, ry = 80; // 레이맨의 현재 좌표
+int rayman_dir = 1;   // 0:왼쪽 1:오른쪽
+int fps = 0; //  frame 카운터
+float fps_time = 1000.0 / 30.0; // 1 frame time
 
 // 팔레트 구조체
 struct Color
@@ -42,6 +45,19 @@ cel_data rayman; // 레이맨 stand
 cel_data back; // 배경
 
 
+void Timer(int Value)
+{
+    fps++; // frame 카운트
+    if (fps > 65535) fps = 0;     // 65535보다 커지면 0으로 초기화
+
+    if (rx <= 200) rx++;
+    else rx = 80;
+
+    glutPostRedisplay(); // dokeyboard 있는 것은 삭제
+    glutTimerFunc(fps_time, Timer, 1); // 타이머 재호출
+}
+
+
 // 키 체크 관련 함수 -------------------------------------
 
 // 일반 키 체크 함수
@@ -57,10 +73,12 @@ void DoKeyboard(int key, int x, int y)
     if (key == GLUT_KEY_LEFT) // 왼쪽 키
     {
         rx -= 1;
+        rayman_dir = 0;
     }
     if (key == GLUT_KEY_RIGHT) // 오른쪽 키
     {
         rx += 1;
+        rayman_dir = 1;
     }
     if (key == GLUT_KEY_UP) // 위쪽 키
     {
@@ -184,12 +202,13 @@ void set_pixel(int x, int y, unsigned char color)
 
 
 // x, y 좌표에 cel그림 그리는 함수
-void put_cel(int x, int y, cel_data* cel)
+void put_cel(int dir, int x, int y, cel_data* cel)
 {
     int count = 0;
     for (int i = 0; i < cel->height; i++) {
         for (int j = 0; j < cel->width; j++) {
-            set_pixel(x + j, y + i, cel->imageData[count]);
+            if (dir == 0) set_pixel(x - j + cel->width, y + i, cel->imageData[count]); // 왼쪽 출력
+            else set_pixel(x + j, y + i, cel->imageData[count]); // 오른쪽 출력
             count++;
         }
     }
@@ -208,12 +227,12 @@ void Render()
     glBegin(GL_POINTS); // 그래픽 시작
 
     // 배경 출력
-    put_cel(0, 0, &back);
+    put_cel(1, 0, 0, &back);
 
     // 레이맨 출력
-    put_cel(rx, ry, &rayman);
+    put_cel(rayman_dir, rx, ry, &rayman);
 
-    glEnd();            // 그래픽 끝
+    glEnd(); // 그래픽 끝
 
     glFlush();  // 지금까지의 내용을 실제 출력
 }
@@ -244,6 +263,7 @@ int main()
 
     glClearColor(0.0, 0.0, 0.0, 0.0);  // R,G,B,A 배경:파란색
 
+    glutTimerFunc(fps_time, Timer, 1); //타이머: fps초 뒤에 Timer 함수 실행
 
     // 키 체크
     glutKeyboardFunc(Keyboard); // 일반 키 체크
